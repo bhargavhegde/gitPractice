63035faabba213725249b05202300c07a927e161
G PAVITHRAN <pavithran@Gs-MacBook-Air.lan>
2026-02-02 20:04:32 -0500

    feature mul and divide

--- calculaotr.py
+++ calculaotr.py
@@ -1,3 +1,12 @@
 class Calculator:
         def __init__(self):
             print("Calculator initialized")
+
+        def multiply(self, a, b):
+            return a * b
+
+        def divide(self, a, b):
+            return a / b
+
+obj=Calculator()
+print(obj.multiply(10, 20))
