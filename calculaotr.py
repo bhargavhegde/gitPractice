a14be84a0d86aa0b27c8616a83968f087479e34d
Rishab Darshan Shylendra <rishabdarshanshylendra@Mac.lan>
2026-02-02 20:00:53 -0500

    Add addition and subtraction functions

--- calculaotr.py
+++ calculaotr.py
@@ -1,3 +1,15 @@
 class Calculator:
         def __init__(self):
             print("Calculator initialized")
+
+        def add(self, a, b):
+              sum = 0
+              sum = a+b
+
+              return sum
+
+        def subtract(self, a, b):
+               diff = 0 
+               diff = a-b 
+               
+               return diff
